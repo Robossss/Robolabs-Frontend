c1a0406ff6e128da9cee68142b0146a1bf14820d
Nii <niiodartey10@gmail.com>
2023-07-19 13:49:00 +0100

    Add visibility functionality for password fields

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import {AiFillEye,AiFillEyeInvisible} from "react-icons/ai"
 export interface InputInterface {
   img: string;
   name: "firstName" | "lastName" | "username" | "password" |"confirmPassword";
@@ -7,13 +8,17 @@ export interface InputInterface {
 }
 
 const Input = React.forwardRef<HTMLInputElement,InputInterface>((props, ref) => {
-  
+  const [show,setShow]= useState(false)
   return (
     <div className="bg-white px-2 h-14 w-96 gap-4 items-center rounded-lg flex ">
       <i>
         <Image height={20} width={20} src={props.img} alt="icon" />
       </i>
-      <input  type={props.name==='password' || props.name==='confirmPassword' ? "password":undefined} {...props} ref={ref} className="text-black w-full focus:outline-none" />
+      <input  type={show?'text':props.name==='password' || props.name==='confirmPassword' ? "password":show?'text':undefined} {...props} ref={ref} className="text-black w-full focus:outline-none" />
+      {
+        (props.name==='password' || props.name==='confirmPassword') &&
+      <i className="text-black" onClick={()=>setShow(!show)}>{show?<AiFillEyeInvisible/>:<AiFillEye/>}</i>
+      }
     </div>
   );
 });
